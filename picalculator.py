24c464a0ee779ec26b1404198a2687741db92647
oz-dahlia-oct <oz.dahlia.oct@gmail.com>
2022-08-15 19:04:10 +0900

    fixed multiprocessing

--- picalculator.py
+++ picalculator.py
@@ -1,106 +1,91 @@
 import math
-from multiprocessing.dummy import Process
 import time
 from threading import Thread
+from multiprocessing import Process
 
 
-class PiCalculator:
+from tqdm import tqdm
 
 
-    def __init__(self, notebook=True):
-        if notebook:
-            from tqdm.notebook import tqdm
-            self.tqdm = tqdm
-        else:
-            from tqdm import tqdm
-            self.tqdm = tqdm
+def calculate_pi():
+    '''ガウス＝ルジャンドルのアルゴリズムに従って、近似円周率を計算します
+    '''
 
-        self.threads = []
-        self.processes = []
+    a = {'n': 1, 'n+1': None}
+    b = {'n': 1 / math.sqrt(2), 'n+1': None}
+    t = {'n': 1 / 4, 'n+1': None}
+    p = {'n': 1, 'n+1': None}
 
+    for _ in range(1000):
+        a['n+1'] = (a['n'] + b['n']) / 2
+        b['n+1'] = math.sqrt(a['n'] * b['n'])
+        t['n+1'] = t['n'] - p['n'] * (a['n'] - a['n+1'])**2
+        p['n+1'] = 2 * p['n']
+        a['n'] = a['n+1']
+        b['n'] = b['n+1']
+        t['n'] = t['n+1']
+        p['n'] = p['n+1']
 
-    def __calculate(self):
-        '''ガウス＝ルジャンドルのアルゴリズムに従って、近似円周率を計算します
-        '''
-
-        a = {'n': 1, 'n+1': None}
-        b = {'n': 1 / math.sqrt(2), 'n+1': None}
-        t = {'n': 1 / 4, 'n+1': None}
-        p = {'n': 1, 'n+1': None}
-
-        for _ in range(1000):
-            a['n+1'] = (a['n'] + b['n']) / 2
-            b['n+1'] = math.sqrt(a['n'] * b['n'])
-            t['n+1'] = t['n'] - p['n'] * (a['n'] - a['n+1'])**2
-            p['n+1'] = 2 * p['n']
-            a['n'] = a['n+1']
-            b['n'] = b['n+1']
-            t['n'] = t['n+1']
-            p['n'] = p['n+1']
-
-        pi = (a['n'] + b['n'])**2 / 4 / t['n']
-
-        return pi
+    pi = (a['n'] + b['n'])**2 / 4 / t['n']
+    return pi
 
 
-    def __calculate_3000(self):
+def calculate_pi_3000():
 
-        for _ in self.tqdm(range(3000)):
-            pi = self.__calculate()
+    for _ in tqdm(range(3000)):
+        calculate_pi()
 
-        return pi
-
-
-    def calculate(self, num=7):
-
-        start_time = time.time()
-
-        for _ in range(num):
-            pi = self.__calculate_3000()
-
-        print('total time:', time.time() - start_time)
-        print()
-
-        return pi
+    return 
 
 
-    def calculate_with_threads(self, num=7):
+def calculate_serialy(num=7):
 
-        start_time = time.time()
+    start_time = time.time()
 
-        for _ in range(num):
-            self.threads.append(
-                Thread(target=self.__calculate_3000)
-            )
+    for _ in range(num):
+        calculate_pi_3000()
 
-        for t in self.threads:
-            t.start()
-
-        for t in self.threads:
-            t.join()
-
-        print('total time:', time.time() - start_time)
-        print()
-
-        return 
+    print('total time:', time.time() - start_time)
+    print()
+    return
 
 
-    def calculate_with_processes(self, num=7):
+def calculate_with_threads(num=7):
 
-        start_time = time.time()
+    start_time = time.time()
 
-        for _ in range(num):
-            self.processes.append(
-                Process(target=self.__calculate_3000)
-            )
+    threads = []
+    for _ in range(num):
+        threads.append(
+            Thread(target=calculate_pi_3000)
+        )
 
-        for p in self.processes:
-            p.start()
+    for t in threads:
+        t.start()
 
-        for p in self.processes:
-            p.join()
+    for t in threads:
+        t.join()
 
-        print('total time:', time.time() - start_time)
-        print()
+    print('total time:', time.time() - start_time)
+    return 
 
-        return 
+
+def calculate_with_processes(num=7):
+
+    start_time = time.time()
+
+    processes = []
+    for _ in range(num):
+        processes.append(
+            Process(target=calculate_pi_3000)
+        )
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print('total time:', time.time() - start_time)
+    print()
+    return 
--- speed_test.py
+++ speed_test.py
@@ -1,9 +1,9 @@
-from picalculator import PiCalculator
+from picalculator import calculate_serialy, calculate_with_threads, calculate_with_processes
+
 
 if __name__ == '__main__':
     
-    calculator = PiCalculator(notebook=False)
-    calculator.calculate()                  # 直列的に7回処理を行う
-    calculator.calculate_with_threads()     # 7つの Thread を作成し、並行処理を行う
-    calculator.calculate_with_processes()   # 7つの Process を作成し、並列処理を行う
+    calculate_serialy()          # 直列的に7回処理を行う
+    calculate_with_threads()     # 7つの Thread を作成し、並行処理を行う
+    calculate_with_processes()   # 7つの Process を作成し、並列処理を行う
     
